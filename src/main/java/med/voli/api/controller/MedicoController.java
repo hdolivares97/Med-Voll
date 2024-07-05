e8d6f94bcacc4b52218d9e7fdedc6acec8a8a54c
Hugo Olivares <hdolivares97@gmail.com>
2024-07-05 00:51:05 -0600

    Clase 5

--- src/main/java/med/voli/api/controller/MedicoController.java
+++ src/main/java/med/voli/api/controller/MedicoController.java
@@ -1,10 +1,8 @@
 package med.voli.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voli.api.medico.DatosListadoMedico;
-import med.voli.api.medico.DatosRegistroMedico;
-import med.voli.api.medico.Medico;
-import med.voli.api.medico.MedicoRepository;
+import med.voli.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,6 +26,27 @@ public class MedicoController {
 
     @GetMapping
     public Page<DatosListadoMedico> listarMedicos(Pageable paginacion){
-        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+        //return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
     }
+
+    @PutMapping
+    @Transactional
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+    }
+
+    //DELETE LOGICO
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void eliminarMedico(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+    // DELETE EN BASE DE DATOS
+    //    public void eliminarMedico(@PathVariable Long id){
+//        Medico medico = medicoRepository.getReferenceById(id);
+//        medicoRepository.delete(medico);
+//    }
 }
