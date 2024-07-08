765c0a3ed3cb00351d78872c0b69e7402407b013
Hugo Olivares <hdolivares97@gmail.com>
2024-07-08 00:48:30 -0600

    Curso 2 - clase 4

--- src/main/java/med/voli/api/controller/AutenticacionController.java
+++ src/main/java/med/voli/api/controller/AutenticacionController.java
@@ -1,27 +1,37 @@
 package med.voli.api.controller;
 
+import jakarta.validation.Valid;
 import med.voli.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voli.api.domain.usuarios.Usuario;
+import med.voli.api.infra.security.DatosJWTToken;
+import med.voli.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("login")
+@RequestMapping("/login")
 public class AutenticacionController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
-    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
-        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
     }
 }
---
+++ src/main/java/med/voli/api/infra/security/DatosJWTToken.java
@@ -0,0 +1,4 @@
+package med.voli.api.infra.security;
+
+public record DatosJWTToken(String JWTtoken) {
+}
